8234de19d52bf12e328c57f5d8fe147927e4de0f
Shift <shift@laravelshift.com>
2021-10-04 08:41:11 +0000

    Convert options array to fluent methods
    Laravel 8 adopts the tuple syntax for controller actions. Since the old options array is incompatible with this syntax, Shift converted them to use modern, fluent methods.

--- routes/web.php
+++ routes/web.php
@@ -10,41 +10,41 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', ['as' => 'jackpot', 'uses' => 'JackpotController@index']);
-Route::get('/pvp', ['as' => 'pvp', 'uses' => 'PvpController@index']);
-Route::get('/double', ['as' => 'double', 'uses' => 'DoubleController@index']);
-Route::get('/battle', ['as' => 'battle', 'uses' => 'BattleController@index']);
+Route::get('/', 'JackpotController@index')->name('jackpot');
+Route::get('/pvp', 'PvpController@index')->name('pvp');
+Route::get('/double', 'DoubleController@index')->name('double');
+Route::get('/battle', 'BattleController@index')->name('battle');
 
-Route::get('/history/jackpot', ['as' => 'jackpotHistory', 'uses' => 'HistoryController@jackpot']);
-Route::get('/history/double', ['as' => 'doubleHistory', 'uses' => 'HistoryController@double']);
-Route::get('/history/battle', ['as' => 'battleHistory', 'uses' => 'HistoryController@battle']);
-Route::get('/history/pvp', ['as' => 'pvpHistory', 'uses' => 'HistoryController@pvp']);
+Route::get('/history/jackpot', 'HistoryController@jackpot')->name('jackpotHistory');
+Route::get('/history/double', 'HistoryController@double')->name('doubleHistory');
+Route::get('/history/battle', 'HistoryController@battle')->name('battleHistory');
+Route::get('/history/pvp', 'HistoryController@pvp')->name('pvpHistory');
 
-Route::get('/partnership', ['as' => 'partnership', 'uses' => 'PagesController@partnership']);
-Route::get('/help', ['as' => 'help', 'uses' => 'PagesController@help']);
-Route::get('/rules', ['as' => 'rules', 'uses' => 'PagesController@rules']);
-Route::get('/fair', ['as' => 'fair', 'uses' => 'PagesController@fair']);
-Route::get('/fair/{hash}', ['as' => 'fairGame', 'uses' => 'PagesController@fairGame']);
+Route::get('/partnership', 'PagesController@partnership')->name('partnership');
+Route::get('/help', 'PagesController@help')->name('help');
+Route::get('/rules', 'PagesController@rules')->name('rules');
+Route::get('/fair', 'PagesController@fair')->name('fair');
+Route::get('/fair/{hash}', 'PagesController@fairGame')->name('fairGame');
 Route::post('/fair/check', 'PagesController@fairCheck');
 Route::get('/result', 'PagesController@result');
 Route::get('/success', 'PagesController@success');
 Route::get('/fail', 'PagesController@fail');
 
-Route::group(['prefix' => '/auth'], function () {
+Route::prefix('/auth')->group(function () {
     Route::get('/telegram/callback', 'AuthController@handleTelegramCallback')->name('auth.telegram.handle');
 });
 
-Route::group(['middleware' => 'auth'], function () {
-    Route::group(['prefix' => 'roulette'], function () {
+Route::middleware('auth')->group(function () {
+    Route::prefix('roulette')->group(function () {
         Route::post('/addBet', 'DoubleController@addBet');
         Route::post('/getBet', 'DoubleController@getBet');
         Route::get('/history', 'DoubleController@history');
     });
-    Route::get('/logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
-    Route::get('/referral', ['as' => 'ref', 'uses' => 'PagesController@referral']);
-    Route::get('/bonus', ['as' => 'bonus', 'uses' => 'PagesController@bonus']);
-    Route::get('/pay', ['as' => 'pay', 'uses' => 'PagesController@pay']);
-    Route::get('/pay/history', ['as' => 'payhistory', 'uses' => 'PagesController@payHistory']);
+    Route::get('/logout', 'AuthController@logout')->name('logout');
+    Route::get('/referral', 'PagesController@referral')->name('ref');
+    Route::get('/bonus', 'PagesController@bonus')->name('bonus');
+    Route::get('/pay', 'PagesController@pay')->name('pay');
+    Route::get('/pay/history', 'PagesController@payHistory')->name('payhistory');
     Route::get('/withdraw/cancel/{id}', 'PagesController@withdraw_cancel');
     Route::post('/withdraw', 'PagesController@withdraw');
     Route::post('/ref/activate', 'PagesController@refActivate');
@@ -57,17 +57,17 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('/flip/joinRoom', 'PvpController@joinRoom');
 });
 
-Route::group(['middleware' => 'auth', 'middleware' => 'access:admin'], function () {
-    Route::get('/admin', ['as' => 'admin', 'uses' => 'AdminController@index']);
-    Route::get('/admin/botOn', ['as' => 'botOn', 'uses' => 'AdminController@botOn']);
-    Route::get('/admin/botOff', ['as' => 'botOff', 'uses' => 'AdminController@botOff']);
-    Route::get('/admin/users', ['as' => 'adminUsers', 'uses' => 'AdminController@users']);
-    Route::get('/admin/bots', ['as' => 'adminBots', 'uses' => 'AdminController@bots']);
-    Route::get('/admin/user/{id}', ['as' => 'adminUser', 'uses' => 'AdminController@user']);
-    Route::get('/admin/settings', ['as' => 'adminSettings', 'uses' => 'AdminController@settings']);
-    Route::get('/admin/withdraw', ['as' => 'adminWithdraw', 'uses' => 'AdminController@withdraw']);
-    Route::get('/admin/bonuses', ['as' => 'adminBonus', 'uses' => 'AdminController@bonus']);
-    Route::get('/admin/promo', ['as' => 'adminPromo', 'uses' => 'AdminController@promo']);
+Route::middleware('access:admin')->group(function () {
+    Route::get('/admin', 'AdminController@index')->name('admin');
+    Route::get('/admin/botOn', 'AdminController@botOn')->name('botOn');
+    Route::get('/admin/botOff', 'AdminController@botOff')->name('botOff');
+    Route::get('/admin/users', 'AdminController@users')->name('adminUsers');
+    Route::get('/admin/bots', 'AdminController@bots')->name('adminBots');
+    Route::get('/admin/user/{id}', 'AdminController@user')->name('adminUser');
+    Route::get('/admin/settings', 'AdminController@settings')->name('adminSettings');
+    Route::get('/admin/withdraw', 'AdminController@withdraw')->name('adminWithdraw');
+    Route::get('/admin/bonuses', 'AdminController@bonus')->name('adminBonus');
+    Route::get('/admin/promo', 'AdminController@promo')->name('adminPromo');
     Route::get('/admin/user/delete/{id}', 'AdminController@userDelete');
     Route::post('/admin/userSave', 'AdminController@userSave');
     Route::post('/admin/usersAjax', 'AdminController@usersAjax');
@@ -95,16 +95,16 @@ Route::group(['middleware' => 'auth', 'middleware' => 'access:admin'], function 
 });
 
 Route::group(['middleware' => 'auth', 'middleware' => 'moder:Moder'], function () {
-    Route::get('/admin', ['as' => 'admin', 'uses' => 'AdminController@index']);
-    Route::get('/admin/promo', ['as' => 'adminPromo', 'uses' => 'AdminController@promo']);
+    Route::get('/admin', 'AdminController@index')->name('admin');
+    Route::get('/admin/promo', 'AdminController@promo')->name('adminPromo');
     Route::post('/admin/promoNew', 'AdminController@promoNew');
     Route::post('/admin/promoSave', 'AdminController@promoSave');
     Route::get('/admin/promoDelete/{id}', 'AdminController@promoDelete');
     Route::post('/chatdel', 'ChatController@delete_message');
 });
 
-Route::group(['prefix' => 'api', 'middleware' => 'secretKey'], function () {
-    Route::group(['prefix' => 'jackpot'], function () {
+Route::prefix('api')->middleware('secretKey')->group(function () {
+    Route::prefix('jackpot')->group(function () {
         Route::post('/newGame', 'JackpotController@newGame');
         Route::post('/getSlider', 'JackpotController@getSlider');
         Route::post('/getStatus', 'JackpotController@getStatus');
@@ -112,14 +112,14 @@ Route::group(['prefix' => 'api', 'middleware' => 'secretKey'], function () {
         Route::post('/getRooms', 'JackpotController@getRooms');
         Route::post('/addBetFake', 'JackpotController@newBetFake');
     });
-    Route::group(['prefix' => 'roulette'], function () {
+    Route::prefix('roulette')->group(function () {
         Route::post('/getGame', 'DoubleController@getGame');
         Route::post('/updateStatus', 'DoubleController@updateStatus');
         Route::post('/getSlider', 'DoubleController@getSlider');
         Route::post('/newGame', 'DoubleController@newGame');
         Route::post('/addBetFake', 'DoubleController@addBetFake');
     });
-    Route::group(['prefix' => 'battle'], function () {
+    Route::prefix('battle')->group(function () {
         Route::post('/newGame', 'BattleController@newGame');
         Route::post('/getSlider', 'BattleController@getSlider');
         Route::post('/getStatus', 'BattleController@getStatus');
